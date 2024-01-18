6ff12ff0a5ddea2c992ffcfd484dfd5dcae9427d
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-01-18 20:54:57 +0200

    Add tapDropDownButton to auth flow test

--- test/src/features/authentication/auth_flow_test.dart
+++ test/src/features/authentication/auth_flow_test.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/more_menu_button.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../robot.dart';
@@ -7,5 +8,7 @@ void main() {
     final r = Robot(tester);
     await r.pumbMyApp();
     await r.expectFindAllProductsCard();
+    await r.tapDropDownButton();
+    expect(find.byKey(MoreMenuButton.signInKey), findsOneWidget);
   });
 }
--- test/src/robot.dart
+++ test/src/robot.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/more_menu_button.dart';
 import 'package:ecommerce_app/src/features/products/presentation/products_list/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -30,5 +31,15 @@ class Robot {
   Future<void> expectFindAllProductsCard() async {
     final productCardFinder = find.byType(ProductCard);
     expect(productCardFinder, findsNWidgets(kTestProducts.length));
+  }
+
+  Future<void> tapDropDownButton() async {
+    final finder = find.byType(MoreMenuButton);
+    // we want to interact with this widget if it is found so we use evaluate
+    final matches = finder.evaluate();
+    if (matches.isNotEmpty) {
+      await tester.tap(finder);
+      await tester.pumpAndSettle();
+    }
   }
 }
